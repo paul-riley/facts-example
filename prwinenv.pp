3942cd581c48bd1ce6fb8fa938c60389146398cf
student8 <student8@puppet.com>
2021-07-20 12:04:59 -0600

    added prwinenv.pp

---
+++ prwinenv.pp
@@ -0,0 +1,27 @@
+class profile::prwinenv::prwinenv{
+  notify {'Running in prwinenv':}
+
+  file { 'c:/test':
+    ensure => directory,
+  }
+
+  windows_env {'PRCUSTOM':
+    ensure    => present,
+    variable  => 'prvariable',
+    value     => 'pr-value',
+    mergemode => clobber,
+  }
+  notify {'PRCUSTOM windows_env has been set!':}
+
+  windows_env { 'CBI_ENV':
+    ensure    => present,
+    variable  => 'CBI_ENV',
+    value     => 'CBI value is here',
+    mergemode => insert,
+  }
+  notify {'CBI_ENV windows_env has been set!':}
+
+  windows_env {'PATH=C:\test':}
+  $prpath = $facts['windows_env']['HOMEPATH']
+  notify {'PATH for windows_env has been appended!':}
+}
